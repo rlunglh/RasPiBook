7470327500c9df3e6e1264cce24d3af644bebb17
rlunglh <rlunglh@users.noreply.github.com>
2017-08-29 11:12:15 -0400

    Add files via upload
    Script to install packages on the mt7688 used in the Book examples

---
+++ software.sh
@@ -0,0 +1,42 @@
+opkg update
+opkg install espeak
+opkg install kmod-usb-audio
+opkg install lftp openssh-sftp-server openssh-sftp-client
+opkg install libcares
+opkg install mosquitto-client
+opkg install nano
+opkg install portaudio
+opkg install ser2net
+opkg install ntpd
+opkg install usbutils
+opkg install wget
+opkg install fswebcam
+opkg install v4l-utils libv4l
+opkg install kmod-video-uvc
+opkg install kmod-usb-audio kmod-sound-core
+opkg install madplay-alsa
+opkg install mpd-full
+opkg install sox
+rm picoc
+wget http://www.microimagesys.com/Downloads/linkit/picoc
+chmod +x picoc
+rm clkHelper
+wget http://www.microimagesys.com/Downloads/linkit/clkHelper
+chmod +x clkHelper
+rm BellToll.wav
+wget http://www.microimagesys.com/Downloads/linkit/BellToll.wav
+rm duoFlicker.c
+wget http://www.microimagesys.com/Downloads/linkit/duoFlicker.c
+rm flicker.c
+wget http://www.microimagesys.com/Downloads/linkit/flicker.c
+rm pkill
+wget http://www.microimagesys.com/Downloads/linkit/pkill
+chmod +x pkill
+cp pkill /usr/bin
+rm pngRAW
+wget http://www.microimagesys.com/Downloads/linkit/pngRAW
+chmod +x pngRAW
+rm arduino_icon.raw
+wget http://www.microimagesys.com/Downloads/linkit/arduino_icon.raw
+
+
